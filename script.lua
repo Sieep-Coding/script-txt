31b84782c21a21b1c9ba32ff1642b64d6e5e484b
Sieep-Coding <nickstambaugh@proton.me>
2024-10-03 16:34:47 -0400

    formatting

--- script.lua
+++ script.lua
@@ -3,20 +3,20 @@ local output = 'accessions.csv'
 -- local csv = require("csv")
 
 local function read_file(lines)
-local file = io.open(input, "rb")
-if not file then
-    error("Not the correct input file")
-    return nil
-elseif file then
-    print("Accepted txt")
-    local lines = file:read("*a")
-    file:close()
-    return lines
-end
+    local file = io.open(input, "rb")
+    if not file then
+        error("Not the correct input file")
+        return nil
+    elseif file then
+        print("Accepted txt")
+        local lines = file:read("*a")
+        file:close()
+        return lines
+    end
 end
 
 local content = read_file(input)
 -- for k,v in pairs(content) do
 --     print('line['.. k .. ']', v)
 -- end
-print("Finished output: ",output, "from input: ",input)
+print("Finished output: ", output, "from input: ", input)
