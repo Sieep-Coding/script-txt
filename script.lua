b935d53b4cfae45271602edb31ad93ea2a03c7f9
Sieep-Coding <nickstambaugh@proton.me>
2024-10-03 16:31:32 -0400

    nice printing, still needs business logic!

--- script.lua
+++ script.lua
@@ -2,16 +2,21 @@ local input = 'accessions.txt'
 local output = 'accessions.csv'
 -- local csv = require("csv")
 
+local function read_file(lines)
 local file = io.open(input, "rb")
 if not file then
     error("Not the correct input file")
     return nil
 elseif file then
     print("Accepted txt")
-    local lines = {}
-    for line in lines:gsub() do
-        lines[#lines + 1] = line
-    end
+    local lines = file:read("*a")
+    file:close()
     return lines
 end
-print("Finished output:", output, "from input:", input)
+end
+
+local content = read_file(input)
+-- for k,v in pairs(content) do
+--     print('line['.. k .. ']', v)
+-- end
+print("Finished output: ",output, "from input: ",input)
